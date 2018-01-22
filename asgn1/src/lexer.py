32139d4f9269081f484834bf340935498361d769
sanketyd <yadavsanket17@yahoo.in>
2018-01-22 16:30:17 +0530

    Started Writing regex rules

--- asgn1/src/lexer.py
+++ asgn1/src/lexer.py
@@ -1,7 +1,8 @@
 #! /usr/bin/env python
 import ply.lex as lex
+import sys
 
-class Lexer(object):
+class Tokens(object):
     def __init__(self):
         self.num_types = 0
         self.num_keywords = 0
@@ -19,14 +20,14 @@ class Lexer(object):
 
     def _get_types(self):
         types = {
-                'INT'             : 'int',
-                'LONG'            : 'long',
-                'FLOAT'           : 'float',
-                'DOUBLE'          : 'double',
-                'CHAR'            : 'char',
-                'VOID'            : 'void',
-                'BOOLEAN'         : 'boolean',
-                'SHORT'           : 'short'
+                'int'             : 'INT',
+                'long'            : 'LONG',
+                'float'           : 'FLOAT',
+                'double'          : 'DOUBLE',
+                'char'            : 'CHAR',
+                'void'            : 'VOID',
+                'boolean'         : 'BOOLEAN',
+                'short'           : 'SHORT'
                 }
         self.num_types = len(types)
         return types
@@ -34,42 +35,42 @@ class Lexer(object):
 
     def _get_keywords(self):
         keywords = {
-                'ABSTRACT'        : 'abstract',
-                'ASSERT'          : 'assert',
-                'BREAK'           : 'break',
-                'BYTE'            : 'byte',
-                'CASE'            : 'case',
-                'CATCH'           : 'catch',
-                'CLASS'           : 'class',
-                'CONST'           : 'const',
-                'CONTINUE'        : 'continue',
-                'DEFAULT'         : 'default',
-                'DO'              : 'do',
-                'ELSE'            : 'else',
-                'EXTENDS'         : 'extends',
-                'FINAL'           : 'final',
-                'FINALLY'         : 'finally',
-                'FOR'             : 'for',
-                'IF'              : 'if',
-                'IMPLEMENTS'      : 'implements',
-                'IMPORT'          : 'import',
-                'INSTANCEOF'      : 'instanceof',
-                'INTERFACE'       : 'interface',
-                'NATIVE'          : 'native',
-                'NEW'             : 'new',
-                'PACKAGE'         : 'package',
-                'PRIVATE'         : 'private',
-                'PROTECTED'       : 'protected',
-                'PUBLIC'          : 'public',
-                'RETURN'          : 'return',
-                'STATIC'          : 'static',
-                'SUPER'           : 'super',
-                'SWITCH'          : 'switch',
-                'THIS'            : 'this',
-                'THROW'           : 'throw',
-                'THROWS'          : 'throws',
-                'TRY'             : 'try',
-                'WHILE'           : 'while'
+                'abstract'        : 'ABSTRACT',
+                'assert'          : 'ASSERT',
+                'break'           : 'BREAK',
+                'byte'            : 'BYTE',
+                'case'            : 'CASE',
+                'catch'           : 'CATCH',
+                'class'           : 'CLASS',
+                'const'           : 'CONST',
+                'continue'        : 'CONTINUE',
+                'default'         : 'DEFAULT',
+                'do'              : 'DO',
+                'else'            : 'ELSE',
+                'extends'         : 'EXTENDS',
+                'final'           : 'FINAL',
+                'finally'         : 'FINALLY',
+                'for'             : 'FOR',
+                'if'              : 'IF',
+                'implements'      : 'IMPLEMENTS',
+                'import'          : 'IMPORT',
+                'instanceof'      : 'INSTANCEOF',
+                'interface'       : 'INTERFACE',
+                'native'          : 'NATIVE',
+                'new'             : 'NEW',
+                'package'         : 'PACKAGE',
+                'private'         : 'PRIVATE',
+                'protected'       : 'PROTECTED',
+                'public'          : 'PUBLIC',
+                'return'          : 'RETURN',
+                'static'          : 'STATIC',
+                'super'           : 'SUPER',
+                'switch'          : 'SWITCH',
+                'this'            : 'THIS',
+                'throw'           : 'THROW',
+                'throws'          : 'THROWS',
+                'try'             : 'TRY',
+                'while'           : 'WHILE'
                 }
         self.num_keywords = len(keywords)
         return keywords
@@ -139,10 +140,59 @@ class Lexer(object):
         separators = self._get_separators()
         keywords = self._get_keywords()
         misc = self._get_misc_words()
-        tokens = list(types.keys()) + list(operators.keys()) + \
-                list(separators.keys()) + list(keywords.keys()) + \
+        tokens = list(types.values()) + list(operators.keys()) + \
+                list(separators.keys()) + list(keywords.values()) + \
                 misc
         return tokens
 
+def main():
+    toks = Tokens()
+    tokens = toks._get_tokens()
+################################ Rules for Tokens #######################################
 
-lexer = Lexer()
+    t_BLOCK_OPENER = r'\{'
+    t_BLOCK_CLOSER = r'\}'
+
+    def t_INT_CONSTANT(t):
+        r'\d+'
+        t.value = int(t.value)
+        return t
+
+    t_STR_CONSTANT = r'\".*?\"'
+    t_CHAR_CONSTANT = r'\".?\"'
+
+    def t_IDENTIFIER(t):
+        r'[a-zA-Z_][a-zA-Z_0-9]*'
+        t.type = toks._get_keywords().get(t.value,'IDENTIFIER')
+        if t.type == 'IDENTIFIER':
+            t.type = toks._get_types().get(t.value,'IDENTIFIER')
+        return t
+
+    def t_error(t):
+        print("Illegal Character '%s'" % t.value[0])
+        t.lexer.skip(1)
+
+    t_ignore = ' \t'
+
+    def t_newline(t):
+        r'\n+'
+        t.lexer.lineno += len(t.value)
+
+#########################################################################################
+
+    code = open(sys.argv[1],"r").read()
+
+    lexer = lex.lex()
+
+    lexer.input(code)
+
+    while True:
+        tok = lexer.token()
+        if not tok:
+            break
+        print(tok)
+
+    print(lexer.lineno)
+
+if __name__ == '__main__':
+    main()
