b77d30e621e3c444b82e17c009a05462cf9fd997
abhipanda4 <abhipanda03@gmail.com>
2018-02-07 04:11:27 +0530

    symbol table completed; IR complete

--- asgn2/src/utilities.py
+++ asgn2/src/utilities.py
@@ -17,12 +17,11 @@ symbol_table = dict()
 
 
 class SymbolTableEntry:
-    def __init__():
+    def __init__(self):
         self.live = False #Made it false
         self.next_use = None
-
-    def add_address_descriptor(): #Added this because we don't need it in per instruction page table
-        self.address_descriptor = set()
+        self.address_descriptor_mem = set() # keeping it, although redundant, to make life easier
+        self.address_descriptor_reg = [0] * 6
 
 
 class Instruction:
@@ -30,8 +29,11 @@ class Instruction:
         self.line_no = int(statement[0].strip())
         self.label_to_be_added = False
         self.inp1 = None
+        self.array_index_i1 = None
         self.inp2 = None
+        self.array_index_i2 = None
         self.out = None
+        self.array_index_o = None
         self.operation = None
         self.instr_type = None
         self.label_name = None
@@ -39,14 +41,104 @@ class Instruction:
         self.jmp_to_label = None
         self.per_inst_next_use = dict()
         self.build(statement)
+        self.populate_per_inst_next_use()
+
+    def add_to_symbol_table(self, symbols):
+        '''
+        Add the symbol into symbol table if not already exists
+        '''
+        for symbol in symbols:
+            if symbol != None and not symbol.isdigit():
+                if symbol not in symbol_table:
+                    symbol_table[symbol] = SymbolTableEntry()
+
+
+    def handle_array_notation(self, symbol):
+        '''
+        Given a symbol, possibly in the form of a[i],
+        split into `a` and index `i`
+        '''
+        index = symbol.find("[")
+        if index != -1:
+            return symbol[:index], symbol[index + 1:-1]
+        else:
+            return symbol, None
+
 
     def build(self, statement):
-        # TODO:
         # analyse the statement and split it
         # add to symbol table
-        # Also intialize per instruction next use details as nextuse = None & live = False
-        pass
+        instr_type = statement[1].strip()
+        if instr_type == "ifgoto":
+            # 10, ifgoto, leq, a, 50, 2
+            self.instr_type = "ifgoto"
+            self.operation = statement[2].strip()
 
+            self.inp1, self.array_index_i1 = self.handle_array_notation(statement[3].strip())
+            self.inp2, self.array_index_i2 = self.handle_array_notation(statement[4].strip())
+            self.add_to_symbol_table([
+                self.inp1, self.array_index_i1,
+                self.inp2, self.array_index_i2
+            ])
+
+            jmp_location = statement[-1].strip()
+            if jmp_location.isdigit():
+                self.jmp_to_line = jmp_location
+            else:
+                self.jmp_to_label = jmp_location
+
+        elif instr_type == "print":
+            # 10, print, variable
+            self.instr_type = "library_func"
+            self.inp1, self.array_index_i1 = self.handle_array_notation(statement[-1].strip())
+            self.add_to_symbol_table([self.inp1, self.array_index_i1])
+
+        elif instr_type == "call":
+            # 10, call, label_name
+            self.instr_type = "func_call"
+            self.jmp_to_label = statement[-1].strip()
+
+        elif instr_type == "label":
+            # 10, label, label_name
+            self.instr_type = "label"
+            self.label_name = statement[-1].strip()
+
+        elif instr_type == "ret":
+            self.instr_type = "return"
+
+        elif instr_type == "=":
+            # 10, =, a, 2
+            self.instr_type = "assignment"
+            self.inp1, self.array_index_i1 = self.handle_array_notation(statement[-1].strip())
+            self.out, self.array_index_o = self.handle_array_notation(statement[2].strip())
+            self.add_to_symbol_table([
+                self.inp1, self.array_index_i1,
+                self.out, self.array_index_o
+            ])
+
+        else:
+            # 10, +, a, a, b
+            self.instr_type = "arithmetic"
+            self.inp1, self.array_index_i1 = self.handle_array_notation(statement[3].strip())
+            self.inp2, self.array_index_i2 = self.handle_array_notation(statement[4].strip())
+            self.out, self.array_index_o = self.handle_array_notation(statement[2].strip())
+            self.add_to_symbol_table([
+                self.inp1, self.array_index_i1,
+                self.inp2, self.array_index_i2,
+                self.out, self.array_index_o
+            ])
+            self.operation = statement[1].strip()
+
+
+    def populate_per_inst_next_use(self):
+        symbols = [
+                self.inp1, self.array_index_i1,
+                self.inp2, self.array_index_i2,
+                self.out, self.array_index_o
+            ]
+        for symbol in symbols:
+            if symbol != None and not symbol.isdigit():
+                self.per_inst_next_use[symbol] = SymbolTableEntry()
 
 
 def read_three_address_code(filename):
@@ -63,7 +155,7 @@ def read_three_address_code(filename):
             IR = Instruction(statement)
             IR_code.append(IR)
             line_no = IR.line_no
-            instr_type = statement[1].strip()
+            instr_type = IR.instr_type
             if instr_type in leader_instructions:
                 if instr_type != "label":
                     line_no += 1
@@ -91,25 +183,23 @@ def next_use(leader, IR_code):
             # print(x.line_no)
         # print()
         for instr in reversed(basic_block):
-            if not isdigit(instr.out) and not instr.out == None:
+            if not instr.out == None and not instr.out.isdigit():
                 instr.per_inst_next_use[instr.out].live = symbol_table[instr.out].live
                 instr.per_inst_next_use[instr.out].next_use = symbol_table[instr.out].next_use
                 symbol_table[instr.out].live = False
                 symbol_table[instr.out].next_use = None
 
-            if not isdigit(instr.inp1) and not instr.inp1 == None:
+            if not instr.inp1 == None and not instr.inp1.isdigit():
                 instr.per_inst_next_use[instr.inp1].live = symbol_table[instr.inp1].live
                 instr.per_inst_next_use[instr.inp1].next_use = symbol_table[instr.inp1].next_use
                 symbol_table[instr.inp1].live = True
                 symbol_table[instr.inp1].next_use = instr.line_no
 
-            if not isdigit(instr.inp2) and not instr.inp2 == None:
+            if not instr.inp2 == None and not instr.inp2.isdigit():
                 instr.per_inst_next_use[instr.inp2].live = symbol_table[instr.inp2].live
                 instr.per_inst_next_use[instr.inp2].next_use = symbol_table[instr.inp2].next_use
                 symbol_table[instr.inp2].live = True
                 symbol_table[instr.inp2].next_use = instr.line_no
-
-
 
 
 leader, IR_code = read_three_address_code("../test/test1.csv")
