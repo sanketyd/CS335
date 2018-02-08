1c7bd4d0e9b45fd1ffc0682c38425db8778610a7
sanketyd <yadavsanket17@yahoo.in>
2018-02-08 20:44:23 +0530

    now updating register descriptor and address descriptor in gen_code.py

--- asgn2/src/get_reg.py
+++ asgn2/src/get_reg.py
@@ -6,6 +6,8 @@ def save_reg_contents(reg):
     for symbol in symbols:
         for location in symbol_table[symbol].address_descriptor_mem:
             print("mov "+ "reg," + str(location))
+        symbol_table[symbol].address_descriptor_reg.remove(reg)
+    reg_descriptor[reg].clear()
 
 
 def get_reg(instr):
@@ -25,16 +27,25 @@ def get_reg(instr):
         R2 = None
         if is_valid_sym(inp2):
             if len(symbol_table[inp2].address_descriptor_reg) != 0:
-                R2 = symbol_table[inp2].address_descriptor_reg[0]
+                R2 = next(iter(symbol_table[inp2].address_descriptor_reg))
 
         # allocate register for inp1
         if is_valid_sym(inp1):
             for reg_name in symbol_table[inp1].address_descriptor_reg:
                 if len(reg_descriptor[reg_name]) == 1 and instr.per_inst_next_use[inp1].next_use == None:
+                    ###############
+                    reg_descriptor[reg_name].remove(inp1)
+                    reg_descriptor[reg_name].add(out)
+                    symbol_table[out].address_descriptor_reg.add(reg_name)
+                    ##############
                     return reg_name, R2, False
 
         for key, value in reg_descriptor.items():
             if len(value) == 0:
+                ###############
+                reg_descriptor[key].add(out)
+                symbol_table[out].address_descriptor_reg.add(key)
+                ##############
                 return key, R2, True
 
         R1 = None
@@ -42,9 +53,12 @@ def get_reg(instr):
         for reg, content in reg_descriptor.items():
             for var in content:
                 n_use = symbol_table[var].next_use
-                if n_use > next_use:
+                if n_use and n_use > next_use:
                     next_use = n_use
                     R1 = reg
         save_reg_contents(R1)
+
+        reg_descriptor[R1].add(out)
+        symbol_table[out].address_descriptor_reg.add(R1)
 
         return R1, R2, True
