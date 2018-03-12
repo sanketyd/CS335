0551ab90b145e33f640f41d75cd283824a04a530
abhipanda4 <abhipanda03@gmail.com>
2018-03-12 18:45:11 +0530

    19.8 & 19.10 done

--- asgn3/src/LALR_parser.py
+++ asgn3/src/LALR_parser.py
@@ -4,6 +4,7 @@ import ply.lex as lex
 import ply.yacc as yacc
 import lexer
 
+# Section 19.8
 def p_ClassDeclaration(p):
     '''
     ClassDeclaration : Modifiers CLASS Identifier Super ClassBody
@@ -50,7 +51,7 @@ def p_FieldDeclaration(p):
 def p_VariableDeclarators(p):
     '''
     VariableDeclarators : VariableDeclarator
-    | VariableDeclarators VariableDeclarator
+    | VariableDeclarators COMMA VariableDeclarator
     '''
 
 def p_VariableDeclarator(p):
@@ -91,9 +92,86 @@ def p_MethodHeader(p):
 def p_MethodDeclarator(p):
     '''
     MethodDeclarator : Identifier L_PAREN R_PAREN
-    | Identifier L_PAREN FormalParametersList R_PAREN
+    | Identifier L_PAREN FormalParameterList R_PAREN
     '''
 
+def p_FormalParametersList(p):
+    '''
+    FormalParameterList : FormalParameter
+    | FormalParameterList COMMA FormalParameter
+    '''
+
+def p_FormalParameter(p):
+    '''
+    FormalParameter : Type VariableDeclaratorId
+    '''
+
+def p_Throws(p):
+    '''
+    Throws : THROWS ClassTypeList
+    '''
+
+def p_ClassTypeList(p):
+    '''
+    ClassTypeList : ClassType
+    | ClassTypeList COMMA ClassType
+    '''
+
+def p_MethodBody(p):
+    '''
+    MethodBody : Block
+    | STMT_TERMINATOR
+    '''
+
+def p_StaticInitializer(p):
+    '''
+    StaticInitializer : STATIC Block
+    '''
+
+def p_ConstructorDeclaration(p):
+    '''
+    ConstructorDeclaration : Modifiers ConstructorDeclarator Throws ConstructorBody
+    | Modifiers ConstructorDeclarator ConstructorBody
+    | ConstructorDeclarator Throws ConstructorBody
+    | ConstructorDeclarator ConstructorBody
+    '''
+
+def p_ConstructorDeclarator(p):
+    '''
+    ConstructorDeclarator : SimpleName L_PAREN FormalParameterList R_PAREN
+    | SimpleName L_PAREN R_PAREN
+    '''
+
+def p_ConstructorBody(p):
+    '''
+    ConstructorBody : BLOCK_OPENER ExplicitConstructorInvocation BlockStatements BLOCK_CLOSER
+    | BLOCK_OPENER ExplicitConstructorInvocation BLOCK_CLOSER
+    | BLOCK_OPENER BlockStatements BLOCK_CLOSER
+    | BLOCK_OPENER BLOCK_CLOSER
+    '''
+
+def p_ExplicitConstructorInvocation(p):
+    '''
+    ExplicitConstructorInvocation : THIS L_PAREN ArgumentList R_PAREN STMT_TERMINATOR
+    | THIS L_PAREN R_PAREN STMT_TERMINATOR
+    | SUPER L_PAREN ArgumentList R_PAREN STMT_TERMINATOR
+    | SUPER L_PAREN R_PAREN STMT_TERMINATOR
+    '''
+
+# Section 19.9 is about Interfaces
+
+# Seection 19.10
+def p_ArrayInitializer(p):
+    '''
+    ArrayInitializer : BLOCK_OPENER VariableInitializers BLOCK_CLOSER
+    | BLOCK_OPENER BLOCK_CLOSER
+    '''
+
+def p_VariableInitializers(p):
+    '''
+    VariableInitializers : VariableInitializer
+    | VariableInitializers COMMA VariableInitializer
+    '''
 
 
 def main():
