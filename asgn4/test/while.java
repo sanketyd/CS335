445fefc73f46f6424c4f52fb7b917dd4455e5b70
sanketyd <yadavsanket17@yahoo.in>
2018-04-02 23:58:25 +0530

    Methods almost done

--- asgn4/test/while.java
+++ asgn4/test/while.java
@@ -1,17 +1,18 @@
 class Wh {
 
-	int main(){
-	while (1)
+    int main(){
+        int raktim = 1;
+        while (raktim < 5)
         {
-        int c, a = 4, b;
-        c =  b * 3 + a;
-        b++;
-        c = a + c + b;
-        c = b << 2;
+            int c, a = 4, b;
+            c =  b * 3 + a;
+            b++;
+            c = a + c + b;
+            c = b << 2;
 
 
         }
 
         /* Copy remaining elements of L[] if any */
-   }
+    }
 }
