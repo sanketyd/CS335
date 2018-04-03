8074cfb598452525e27d04a5e3ea302cbf5c95a0
abhipanda4 <abhipanda03@gmail.com>
2018-04-03 20:29:06 +0530

    Minor corrections

--- asgn4/src/three_address_code.py
+++ asgn4/src/three_address_code.py
@@ -24,7 +24,7 @@ class TAC:
             elif instruction[3] == '=':
                 print(str(line_no) + ", = , " + str(instruction[0]) + ", " + str(instruction[1]))
             elif instruction[0] == 'declare':
-                prinnt("Currently unimplemented: DECLARATION")
+                print("Currently unimplemented: DECLARATION")
             elif instruction[3] in ['and', 'or', 'xor']:
                 print("Currently unimplemented: LOGICAL OPS")
             elif instruction[0] == 'goto':
