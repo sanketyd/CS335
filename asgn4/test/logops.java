b71768e4389e4efbf38640daf83a1cb3d3a31d8c
abhipanda4 <abhipanda03@gmail.com>
2018-04-08 20:47:04 +0530

    minor modififcations

--- asgn4/test/logops.java
+++ asgn4/test/logops.java
@@ -3,11 +3,11 @@ class Debug {
         return a + b;
     }
     int main() {
-        //int a = 20;
         int a = 19;
-	int b = 2;
-	int d = 1 ;
-	boolean c;
-	c = a && b || d;
+        int b = 2;
+        int d = 1 ;
+        boolean c;
+        c = a && b || d;
+        c = ~a;
     }
 }
